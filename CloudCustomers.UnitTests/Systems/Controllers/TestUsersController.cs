22febe70496883eeb81b5042a9ac75539f89a5a4
Ligia Viana <ligia.viana.azevedo@gmail.com>
2023-04-04 07:36:04 -0300

    dotnet6-api-tdd-moq-fluent-assertions-cs

---
+++ CloudCustomers.UnitTests/Systems/Controllers/TestUsersController.cs
@@ -0,0 +1,34 @@
+using CloudCustomers.API.Controllers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace CloudCustomers.UnitTests.Systems.Controllers;
+
+public class TestUsersController
+{
+    [Fact]
+    public async Task Get_OnSuccess_ReturnsStatusCode200()
+    {
+        // Arrange
+        var sut = new UsersController();
+
+        // Act
+        var result = (OkObjectResult)await sut.Get();
+
+        // Assert
+        result.StatusCode.Should().Be(200);
+    }
+
+    [Fact]
+    public async Task Get_OnSuccess_InvokesUserService()
+    {
+        // Arrange
+        var mockUserService = Mock<IUsersService>();
+        var sut = new UsersController(mockUserService.Object);
+
+        // Act
+        var result = (OkObjectResult)await sut.Get();
+    }
+}
